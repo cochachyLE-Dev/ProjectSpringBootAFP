858a8d6900ba419523cc8b6d01015ac920e232dd
Luis Eduardo Cochachi Chamorro <lcochach@emeal.nttdata.com>
2022-08-04 19:26:23 -0500

    2022-08-04

--- Database.sql
+++ Database.sql
@@ -1,10 +1,11 @@
+-- drop table public.person
 create table if not exists public.person
 (
-    type_of_identity_document varchar(5) NOT NULL,
-    identification_number varchar(8) NOT NULL,    
-    date_of_birth date,
+    type_of_identity_document varchar(5) not null,
+    identification_number varchar(8) not null,        
     first_name varchar(125),
     last_name varchar(125),
+    date_of_birth date,
     marital_status varchar(30),
     nationatity varchar(15),
     ubigeo varchar(15)    
@@ -13,43 +14,44 @@ create table if not exists public.person
 alter table Person
 add constraint person_pkey primary key(identification_number, type_of_identity_document);
 
-create table Affiliate(
-    typeOfIdentityDocument varchar(5) not null,
-	identificationNumber varchar(8) not null,     
+-- drop table public.Affiliate
+create table if not exists public.Affiliate(
+    type_of_identity_document varchar(5) not null,
+	identification_number varchar(8) not null,     
     code varchar(15) not null,	    
 	origen varchar(30),
-	typeCommission varchar(30),	
+	type_commission varchar(30),	
 	currentAFP varchar(30),	
-	dateOfLastContribution timestamp,
-	dateOfConsultation timestamp,
-	dateOfEntryToTheSPP timestamp,	
-	typeOfWorker varchar(11), -- // Dependent / Independent	
+	date_of_last_contribution timestamp,
+	date_of_consultation timestamp,
+	date_of_entry_to_theSPP timestamp,	
+	type_of_worker varchar(11), -- // Dependent / Independent	
 	status varchar(20),
-	dateOfAffiliationToCurrentAFP timestamp
+	date_of_affiliation_to_currentAFP timestamp
 );
 
 alter table Affiliate
 add constraint pkAffiliate primary key(code);
 
 alter table Affiliate
-add constraint fkAffiliatePerson foreign key(typeOfIdentityDocument, identificationNumber) references Person(typeOfIdentityDocument, identificationNumber);
+add constraint fkAffiliatePerson foreign key(type_of_identity_document, identification_number) references Person(type_of_identity_document, identification_number);
 
 alter table Affiliate
 drop constraint fkAffiliatePerson;
 
 create table Contribution(
-    affiliateCode varchar(15) not null,	
-	cumulativeContributions decimal(18,5),	
-	withdrawalsAndAccruedFees decimal(18,5),	
-	accumulatedProfitability decimal(18,5),	
-	accumulatedFund decimal(18,5)
+    affiliate_code varchar(15) not null,	
+	cumulative_contributions decimal(18,5),	
+	withdrawals_and_accruedFees decimal(18,5),	
+	accumulated_profitability decimal(18,5),	
+	accumulated_fund decimal(18,5)
 );
 
 alter table Contribution
-add constraint pkContribution primary key(affiliateCode);
+add constraint pkContribution primary key(affiliate_code);
 
 alter table Affiliate
-add constraint fkAffiliateContribution foreign key(code) references Contribution(affiliateCode);
+add constraint fkAffiliateContribution foreign key(code) references Contribution(affiliate_code);
 
 alter table Affiliate
 drop constraint fkAffiliateContribution;
